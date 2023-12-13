2d125817ec4936acf2068f98e3dd846625f788f8
silpakumar <silpakumar20@gmail.com>
2023-12-13 09:31:07 +0530

    finished route login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
@@ -23,6 +25,33 @@ class AuthController extends Controller
 
 
         $token = $user->createToken('myapptoken')->plainTextToken;
+        $response=[
+            'user'=>$user,
+            'token' =>$token
+        ];
+
+        return response($response,201);
+    }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string'
+        ]);
+
+
+        //check email
+        $user= User ::where('email',$fields['email'])->first();
+
+        //check password
+        if(!$user || !Hash::check($fields['password'],$user->password))
+        return response([
+            'message' => 'Bad Creds'
+    
+        ], 401);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
         $response=[
             'user'=>$user,
             'token' =>$token
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/register',[AuthController::class, 'register']);
+Route::post('/login',[AuthController::class, 'login']);
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
