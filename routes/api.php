5683a3d4375b58a18a3c6bddb7cd7ecc118bb5ea
silpakumar <silpakumar20@gmail.com>
2023-12-13 09:53:07 +0530

    finished route index

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
 Route::post('/logout',[AuthController::class, 'logout']);
+
+Route::get('/tasks',[TaskController::class, 'index']);
+
 
 
 
